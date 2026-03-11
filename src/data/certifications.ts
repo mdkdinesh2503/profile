ff35be1a57c0cc6f581fdefb64f83640e8f1ed3c
Dinesh Kumar M <mdkdinesh2503@gmail.com>
2026-03-11 22:37:59 +0530

    Update certifications data by commenting out verify links for existing entries and adding a new certification for Rust Programming Essentials, including issuer and year details.

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -7,7 +7,7 @@ export const certifications: Certification[] = [
     year: "Apr 17, 2025",
     thumbnail: "./default/certificate.svg",
     pdf: "./certification/Google_Prompting_Essentials.pdf",
-    verifyLink: "",
+    // verifyLink: "https://www.coursera.org/account/accomplishments/records/XJITQC4NHDM1",
   },
   {
     name: "Advanced React",
@@ -15,7 +15,7 @@ export const certifications: Certification[] = [
     year: "May 12, 2025",
     thumbnail: "./default/certificate.svg",
     pdf: "./certification/Advanced_React.pdf",
-    verifyLink: "",
+    // verifyLink: "https://www.coursera.org/account/accomplishments/records/XSPWGPG37QZQ",
   },
   {
     name: "AWS DynamoDB",
@@ -23,6 +23,14 @@ export const certifications: Certification[] = [
     year: "Jun 16, 2025",
     thumbnail: "./default/certificate.svg",
     pdf: "./certification/AWS_DynamoDB.pdf",
-    verifyLink: "",
+    // verifyLink: "https://www.coursera.org/account/accomplishments/records/UTXJPVA09XLA",
+  },
+  {
+    name: "Rust Programming Essentials",
+    issuer: "Edureka",
+    year: "Mar 11, 2026",
+    thumbnail: "./default/certificate.svg",
+    pdf: "./certification/Rust_Programming_Essentials.pdf",
+    // verifyLink: "https://www.coursera.org/account/accomplishments/records/Z2HOYRLR8MPC",
   },
 ];
