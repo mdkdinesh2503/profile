7f29e0bc15024e23339cb0fbc0181792113b7669
mdkdinesh2503 <mdkdinesh2503@gmail.com>
2026-07-08 00:08:50 +0530

    feat: add new certifications data and Aspire Systems internship certificate file

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -2,20 +2,12 @@ import type { Certification } from "@/types";
 
 export const certifications: Certification[] = [
   {
-    name: "Google Prompting Essentials",
-    issuer: "Google",
-    year: "Apr 17, 2025",
+    name: "Rust Programming Essentials",
+    issuer: "Edureka",
+    year: "Mar 11, 2026",
     thumbnail: "./default/certificate.svg",
-    pdf: "./certification/Google_Prompting_Essentials.pdf",
-    // verifyLink: "https://www.coursera.org/account/accomplishments/records/XJITQC4NHDM1",
-  },
-  {
-    name: "Advanced React",
-    issuer: "Whizlabs",
-    year: "May 12, 2025",
-    thumbnail: "./default/certificate.svg",
-    pdf: "./certification/Advanced_React.pdf",
-    // verifyLink: "https://www.coursera.org/account/accomplishments/records/XSPWGPG37QZQ",
+    pdf: "./certification/Rust_Programming_Essentials.pdf",
+    // verifyLink: "https://www.coursera.org/account/accomplishments/records/Z2HOYRLR8MPC",
   },
   {
     name: "AWS DynamoDB",
@@ -26,11 +18,26 @@ export const certifications: Certification[] = [
     // verifyLink: "https://www.coursera.org/account/accomplishments/records/UTXJPVA09XLA",
   },
   {
-    name: "Rust Programming Essentials",
-    issuer: "Edureka",
-    year: "Mar 11, 2026",
+    name: "Advanced React",
+    issuer: "Whizlabs",
+    year: "May 12, 2025",
     thumbnail: "./default/certificate.svg",
-    pdf: "./certification/Rust_Programming_Essentials.pdf",
-    // verifyLink: "https://www.coursera.org/account/accomplishments/records/Z2HOYRLR8MPC",
+    pdf: "./certification/Advanced_React.pdf",
+    // verifyLink: "https://www.coursera.org/account/accomplishments/records/XSPWGPG37QZQ",
+  },
+  {
+    name: "Google Prompting Essentials",
+    issuer: "Google",
+    year: "Apr 17, 2025",
+    thumbnail: "./default/certificate.svg",
+    pdf: "./certification/Google_Prompting_Essentials.pdf",
+    // verifyLink: "https://www.coursera.org/account/accomplishments/records/XJITQC4NHDM1",
+  },
+  {
+    name: "Aspire Systems Intern",
+    issuer: "Aspire Systems",
+    year: "Mar 14, 2023",
+    thumbnail: "./default/certificate.svg",
+    pdf: "./certification/Aspire_Systems_Intern.pdf",
   },
 ];
